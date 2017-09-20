131436335c60c9dd8e7dab90f2dbde9945b55f6b
ommyzhang <tdxdxoz@gmail.com>
2017-09-20 15:30:52 +0800

    finish addGroup

--- score/urls.py
+++ score/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
         url(r'^$',views.index, name='index'),
         url(r'^add/$',views.add,name='add'),
+        url(r'^addGroup/$',views.addGroup,name='addGroup'),
         url(r'^user/(?P<cun>.+)/$',views.ud,name='ud'),
         url(r'^users/$',views.us,name='us'),
         url(r'^list/$',views.list,name='list')
--- score/views.py
+++ score/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect,HttpResponse
 from .models import record
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Q
 from operator import itemgetter
 from re import findall,IGNORECASE
 import os
@@ -15,10 +17,12 @@ def index(request):
     lis = []
 
     for man in members:
-        rs = record.objects.filter(who=man.username)
+        #rs = record.objects.filter(who=man.username)
+        rs = record.objects.all();
         s = 0
         for r in rs:
-            s += r.soc
+            if(r.who == man.username) or (('#'+man.username+'#') in r.who):
+                s += r.soc
 
         if (s>0) or (man.email[0]=='0'):
             #print man.first_name
@@ -45,7 +49,12 @@ def index(request):
             nam = ''
             un = ''
 
-    return render(request,'score/rank.html',{'list':lis,'name':nam,'un':un})
+    return render(request,'score/rank.html',{
+        'list':lis,
+        'name':nam,
+        'un':un,
+        'is_staff':request.user.is_staff
+        })
 
 @login_required
 def add(request):
@@ -107,17 +116,54 @@ def add(request):
         'ss':score
         })
 
+@staff_member_required
+def addGroup(request):
+    nam = request.user.first_name 
+    un  = request.user.username
+
+    if request.POST:
+        print request.POST
+        s = "#"
+        print request.POST.getlist('values',[])
+        for p in request.POST.getlist('values',[]):
+            print p
+            s = s + p + '#'
+        rc = record(
+                kind = '#',
+                name = request.POST['title'],
+                detail = (u'记录人: ' + nam).encode('utf-8'),
+                who = s,
+                when = request.POST['date'],
+                soc = int(request.POST['score'])
+                )
+        rc.save();
+
+        return  HttpResponseRedirect("/score/")
+    
+    members = User.objects.exclude(username='hiddenadmin').order_by('email');
+    
+    return render(request,'score/addGroup.html',{
+        'members':members,
+        'name':nam,
+        'un':un
+        });
 
 @login_required
 def ud(request,cun):
     try:
         cu = User.objects.get(username=cun)
     except:
-        return HttpResponse('No '+cun)
+        return HttpResponse(cun)
 
-    ar = record.objects.filter(who=cun).order_by("when")
+    ar1 = record.objects.order_by("when")
     nam = request.user.first_name 
     
+    ar = []
+    for rr in ar1:
+        if (rr.who==cun) or (('#'+cun+'#') in rr.who):
+            ar.append(rr);
+
+
     pub_art = []
     path = '../member/'+cun+'/publicSpace/'
     if os.path.exists(path):
@@ -175,6 +221,25 @@ def us(request):
     us.sort(key=itemgetter('gra'))
     return render(request,'score/users.html',{'us':us,'name':nam})
 
+def getWho(s):
+    if s[0]== '#':
+        names = s[1:-1].split('#')
+        print names
+        ans = ''
+        for name in names:
+            print name
+            ans = ans + ' ' + User.objects.get(username = name).first_name
+        return ans[1:]
+    else:
+        return s
+
+def getName(s):
+    if s[0]== '#':
+        names = s[1:-1].split('#')
+        return "共计%d人"%(len(names))
+    else:
+        return User.objects.get(username = s).first_name
+
 @login_required
 def list(request):
 
@@ -188,8 +253,8 @@ def list(request):
 
     l = [{
             'when':x.when,
-            'who':x.who,
-            'rn':User.objects.get(username = x.who).first_name,
+            'who':getWho(x.who),
+            'rn':getName(x.who), # User.objects.get(username = x.who).first_name,
             'kind':x.kind,
             'name':x.name,
             'detail':x.detail,
